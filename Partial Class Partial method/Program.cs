2d80f19e71b4d09e2bae551aea7dba728b44ac9e
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-07-25 23:15:51 +0530

    Add project files.

---
+++ Partial Class Partial method/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Partial_Class_Partial_method
+{
+    partial class A
+    {
+      public  partial void s();
+    }
+    partial class A
+    {
+        public void show()
+        {
+            Console.WriteLine("Hello :");
+        }
+      public  partial void s()
+        {
+            Console.WriteLine("Hiiii  :");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A obj = new A();
+            obj.show();
+            obj.s();
+        }
+    }
+}
